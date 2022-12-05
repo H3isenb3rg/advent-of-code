bd0791cee2dca53772a294bf1b6081de948fdc18
Matteo Beatrice <matteoe97@gmail.com>
2022-12-05 16:59:07 +0100

    Day 3 part 1

--- 2022/day3/solver.py
+++ 2022/day3/solver.py
@@ -16,13 +16,36 @@ class Day3Solver(Solver):
         return parsed_input
     
     def solve_first(self, is_example: bool = False):
-        pass
-        
-    def first_alg(self, rucksacks):
-        pass
+        if is_example:
+            rucksacks = self.example_input
+        else:
+            rucksacks = self.input
 
+        return self.first_alg(rucksacks)
+
+    def compute_priority(self, char: str):
+        if char == char.upper():
+            offset = ord("A")-27
+        else:
+            offset = ord("a")-1
+
+        return ord(char)-offset
+        
+    def first_alg(self, rucksacks: list[list[str]]):
+        total = 0
+        for sack in rucksacks:
+            int = set(sack[:len(sack)//2]).intersection(set(sack[len(sack)//2:]))
+            total += self.compute_priority(list(int)[0]) if len(int)>0 else 0
+
+        return total
+            
     def solve_second(self, is_example: bool = False):
-        pass
+        if is_example:
+            rucksacks = self.example_input
+        else:
+            rucksacks = self.input
+
+        return self.second_alg(rucksacks)
     
-    def second_alg(self, rucksacks):
+    def second_alg(self, rucksacks: list[list[str]]):
         pass
